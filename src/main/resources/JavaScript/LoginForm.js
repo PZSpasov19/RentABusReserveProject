5db7ebcf85535e9ad04dc8a12f3fd1f361b43a63
PZSpasov19 <PZSpasov19@codingburgas.bg>
2024-03-14 13:26:05 +0200

    Adding a registration/login form button

--- src/main/resources/JavaScript/LoginForm.js
+++ src/main/resources/JavaScript/LoginForm.js
@@ -1,11 +1,3 @@
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var parallaxContainer = document.querySelector('.container');
-    var parallaxOffset = scrollPosition * 0.5;
-
-    parallaxContainer.style.backgroundPositionY = -parallaxOffset + 'px';
-});
-
 document.getElementById("popupBtn").addEventListener("click", function() {
     document.getElementById("popup").style.display = "block";
 });
@@ -14,4 +6,12 @@ document.addEventListener("click", function(event) {
     if (event.target.closest("#popup") === null && event.target.id !== "popupBtn") {
         document.getElementById("popup").style.display = "none";
     }
+});
+
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var parallaxContainer = document.querySelector('.container');
+    var parallaxOffset = scrollPosition * 0.5;
+
+    parallaxContainer.style.backgroundPositionY = -parallaxOffset + 'px';
 });
